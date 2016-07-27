da8523e5202f9a85b51742d192543b163e1a453a
Nakajo <mmendonca@sciere.com.br>
2016-07-27 15:31:02 -0300

    Identifica colisão em 1d

--- js/jogo.js
+++ js/jogo.js
@@ -19,23 +19,22 @@ class ElasticBody {
 	updateUI () {
 		this.div.css({'left':this.x});
 	}	
-}
 
-class Collision {
-
-
-	/*Retorna true se estão em contato */
-
-	static contact (elasticBody1, elasticBody2) {
-		//@TODO
-		return true;
+	/*Retorna true se estão em contato 
+	@FIXME 1 dimensao apenas por enquanto
+	*/
+	contact (elasticBody) {
+		var distanceX = Math.abs(this.x - elasticBody.x);
+		//distanceY = abs(this.y - elasticBody.y);
+		return distanceX <= (this.width+elasticBody.width)/2;		
 	}
+
 	/*Os dois objetos colidem. O método atualiza as velocidades deles */
 	static collision (elasticBody1, elasticBody2) {
 		//@TODO 
 	}
 }
-
+/*
 elasticBody1 = new ElasticBody(0,0);
 elasticBody2 = new ElasticBody(1,0);
 elasticBody3 = new ElasticBody(2,0);
@@ -47,3 +46,4 @@ divBattle = [
 	elasticBody3,
 	elasticBody4,
 ]; 
+*/
