82d7b2e42c4d95bda1eda6cb6d29ae8c32c39cff
Luis Hansen <reescolando@gmail.com>
2017-01-19 15:17:04 -0200

    Inicio dos trabalhos em upload de carinhas próprias

--- js/menu.js
+++ js/menu.js
@@ -41,3 +41,17 @@ function roma () {
 	$('#roma').addClass('selected-vilao');
 	$('#sales').removeClass('selected-vilao');
 }
+
+function previewFile() {
+  var preview = document.getElementById('outImage');
+  var file    = document.querySelector('input[type=file]').files[0];
+  var reader  = new FileReader();
+
+  reader.addEventListener("load", function () {
+    preview.src = reader.result;
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+}
